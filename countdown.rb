27091f2b39f68b4a08c476b03f5ba7bdec2c4438
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-28 13:39:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,12 +2,15 @@
 
 
 def countdown(integer)
-  
   while integer > 0 
     puts "#{integer} SECOND(S)!"
     integer -= 1
-  end
-  
+  end 
   return "HAPPY NEW YEAR!"
-  
 end
+
+
+
+def countdown_with_sleep 
+  
+end 
